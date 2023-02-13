bd547982eddd9b4b7c712bc9c2750e4a4cb051cc
Oleksandr <alex.vovchok@gmail.com>
2023-02-13 23:31:23 +0200

    start task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -9,47 +9,28 @@ const imagesMarkup = createImgsMarkup(galleryItems);
 
 galleryList.insertAdjacentHTML('beforeend', imagesMarkup);
 
+
 function createImgsMarkup(galleryItems) {
 return galleryItems.map(({preview, original, description }) => {
-    return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
+    return `<a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
       alt="${description}"
     />
-  </a>
-</div>`;
+  </a>`;
 }).join('');
 };
 
-galleryList.addEventListener('click', onGalleryListClick);
+let gallery = new SimpleLightbox('.gallery a', {
+  
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
 
-function onGalleryListClick(event) {
-  document.addEventListener("keydown", onEscCloseModal);
-    event.preventDefault();
+});
 
-if(!event.target.classList.contains('gallery__image')) {
-    return;
-}
-
-const currentElement = event.target;
-const parentImage = currentElement.closest('.gallery');
-
-console.log(currentElement.getAttribute("data-source"));
-
-const instance = basicLightbox.create(`
-    <img src="${currentElement.dataset.source}">
-`)
-instance.show()
-
-function onEscCloseModal(event) {
-if(event.code === 'Escape') {
-  instance.close(() => document.removeEventListener("keydown", onEscCloseModal));
-}
-};
-
-};
+gallery.on('show.simplelightbox');
 
 console.log(galleryItems);
